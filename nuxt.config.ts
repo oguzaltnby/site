2355f4cf5778a40cff761d4cd6d07f6bb16ce41b
oguzhan <oguzhanaltunbey3@gmail.com>
2025-04-19 12:19:22 +0300

    Refactor code structure for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,11 +11,8 @@ import loading from "./config/loading"
 import modules from "./config/modules"
 import plugins from "./config/plugins"
 import publicRuntimeConfig from "./config/publicRuntimeConfig"
-import path from "path";
-
-// Specific module options
-import viteOptions from "./config/modules/vite"
 import feed from "./config/modules/feed"
+import path from "path"
 
 // Constants
 const isDev = process.env.NODE_ENV === "development"
@@ -26,18 +23,16 @@ const Config: NuxtConfig = {
   target: "static",
   ssr: !isDev,
 
+  // Webpack 5'i aktif hale getiriyoruz
+  build: {
+    transpile: ['ofetch'],
+    // Webpack 5 kullanımı burada etkinleştiriliyor
+    webpack5: true,
+  },
+
   runtimeConfig: {
     openaiApiKey: process.env.OPENAI_API_KEY,
     public: {}
-  },
-
-  vite: {
-    build: {
-      transpile: ['ofetch'],
-      rollupOptions: {
-        external: ["ofetch"]
-      }
-    }
   },
 
   hooks: {
@@ -45,9 +40,6 @@ const Config: NuxtConfig = {
       fallback: true,
       interval: 2000,
       concurrency: 1,
-      // async done(generator) {
-      //   await generateDone(generator)
-      // },
     },
   },
 
@@ -65,9 +57,6 @@ const Config: NuxtConfig = {
   feed,
   plugins,
   publicRuntimeConfig,
-
-  // Vite modül ayarları
-  vite: viteOptions,
 }
 
 export default Config
